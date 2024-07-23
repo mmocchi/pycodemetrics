bc425a28abcd0f3c6439a0048cbe6c54ee6addd3
mmocchi <akihiro.matsumoto.exe@gmail.com>
2024-07-23 23:12:04 +0900

    radonのラッパーを作成

--- pycodemetrics/python.py
+++ pycodemetrics/python.py
@@ -1,12 +1,20 @@
-from gitclient.gitcli import get_gitlogs, list_git_files
-from gitclient.gitlog_parser import parse_gitlogs
+from gitclient.gitcli import list_git_files
+
+from pycodemetrics.radon_wrapper import (
+    get_block_complexity,
+    get_complexity,
+    get_maintainability_index,
+    get_raw_metrics,
+)
 
 if __name__ == "__main__":
     for git_file_path in list_git_files():
         if not git_file_path.endswith(".py"):
             continue
 
-        rawlog = get_gitlogs(git_file_path)
-        parsed_logs = parse_gitlogs(git_file_path, rawlog)
-        for parsed_log in parsed_logs:
-            print(parsed_log)
+        print(git_file_path)
+        print(get_raw_metrics(git_file_path))
+        print(get_complexity(git_file_path))
+        print(get_block_complexity(git_file_path))
+        print(get_maintainability_index(git_file_path))
+        print("*" * 10)
